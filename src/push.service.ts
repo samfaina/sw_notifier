6f77023e10bce0a25938af79a4034620fc2cb076
Cristina Saez <csaez@itteria.cat>
2019-12-19 13:50:34 +0100

    first commit

--- src/push.service.ts
+++ src/push.service.ts
@@ -1,22 +1,19 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as PushBullet from 'pushbullet';
 
-const DEVICE_ID = 'ujwH6rFcu5IsjCkNuiwQMu';
-const PUSH_BULLET_API_KEY = 'o.DbVxzC8KiqE47oRZwSaWq67PC0P2wKhg';
-
 @Injectable()
 export class PushService {
   private pusher: PushBullet;
   private logger = new Logger('PushService');
 
   constructor() {
-    this.pusher = new PushBullet(PUSH_BULLET_API_KEY);
+    this.pusher = new PushBullet(process.env.SW_PUSH_API_KEY);
   }
 
   notifyUpdate(siteName: string): Promise<string> {
     return new Promise((resolve, reject) => {
       this.pusher.note(
-        DEVICE_ID,
+        process.env.SW_PUSH_DEVICE_ID,
         'Yuju!',
         'There are some updates!',
         (error, response) => {
